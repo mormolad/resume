8dea8214e630f28c0ccaaaecdb38ccd7a6ef9b49
Перевалов Антон <paspean@mail.ru>
2025-03-23 16:48:55 +0700

    [~]FB хеадер готов

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,55 +1,60 @@
 'use client';
 import React, { useState } from 'react';
-import styles from './Header.module.css'; // Импортируем CSS-модуль
+import styles from './Header.module.css';
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  // Функция для переключения состояния меню
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
   return (
     <header className={styles.header}>
-      {/* Бургер-кнопка */}
+      {/* Контейнер с бургером и меню */}
       <div
-        className={`${styles.burger} ${isOpen ? styles.open : ''}`}
+        className={`${styles.burgerMenu} ${isOpen ? styles.open : ''}`}
         onClick={toggleMenu}
       >
-        <span></span>
-      </div>
+        {/* Иконка бургера / крестика */}
+        <div className={styles.burgerIcon}>
+          <span></span>
+        </div>
 
-      {/* Навигационное меню */}
-      <nav className={`${styles.nav} ${isOpen ? styles.open : ''}`}>
-        <ul className={styles.nav__list}>
-          <li className={styles.nav__item}>
-            <a href="#" className={styles.nav__link}>
-              Home
-            </a>
-          </li>
-          <li className={styles.nav__item}>
-            <a href="#" className={styles.nav__link}>
-              About
-            </a>
-          </li>
-          <li className={styles.nav__item}>
-            <a href="#" className={styles.nav__link}>
-              Services
-            </a>
-          </li>
-          <li className={styles.nav__item}>
-            <a href="#" className={styles.nav__link}>
-              Portfolio
-            </a>
-          </li>
-          <li className={styles.nav__item}>
-            <a href="#" className={styles.nav__link}>
-              Contact
-            </a>
-          </li>
-        </ul>
-      </nav>
+        {/* Навигационное меню (плавное раскрытие) */}
+        <nav className={`${styles.nav} ${isOpen ? styles.open : ''}`}>
+          <ul className={styles.nav__list}>
+            <li className={styles.nav__item}>
+              <a href="/" className={styles.nav__link}>
+                В начало
+              </a>
+            </li>
+            <li className={styles.nav__item}>
+              <a
+                href="https://github.com/mormolad"
+                className={styles.nav__link}
+              >
+                Портфолио
+              </a>
+            </li>{' '}
+            <li className={styles.nav__item}>
+              <a href="#" className={styles.nav__link}>
+                Анимация
+              </a>
+            </li>
+            <li className={styles.nav__item}>
+              <a href="#" className={styles.nav__link}>
+                Обо мне
+              </a>
+            </li>
+            <li className={styles.nav__item}>
+              <a href="#" className={styles.nav__link}>
+                Контакты
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
